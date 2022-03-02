a4fc17a8ca395c89d5c5b22a55833253ab08a140
KaydanYard <86071974+KaydanYard@users.noreply.github.com>
2022-03-02 07:54:40 -0700

    good

---
+++ src/app/components/departments/departments.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Department } from 'src/app/interfaces/department';
+import { DepartmentsService } from 'src/app/services/departments.service';
+
+@Component({
+  selector: 'app-departments',
+  templateUrl: './departments.component.html',
+  styleUrls: ['./departments.component.scss']
+})
+export class DepartmentsComponent implements OnInit {
+  departments: Department[] | undefined;
+
+  constructor(
+    private departmentsService: DepartmentsService
+  ) { }
+
+  ngOnInit(): void {
+    this.departments = this.departmentsService.departments;
+  }
+
+}
